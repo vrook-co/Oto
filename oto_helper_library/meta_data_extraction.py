afdfd798bc1d2706fb671cbbf8c65ea36a5720ed
Jashwanth Gowda <56092435+Jashwanth-Gowda-R@users.noreply.github.com>
2020-10-30 15:38:18 +0530

    Update meta_data_extraction.py

--- oto_helper_library/meta_data_extraction.py
+++ oto_helper_library/meta_data_extraction.py
@@ -1,25 +1,23 @@
 """
 
-This is a helper function that is meant to be used to download videos for cloud sources such as OneDrive, DropBox, Google-Drive, etc. 
+When an video file is passed along with the name of an output file, exrtact the meta data information of the passed video and write it out to the file.
 
 """
 
 
-def download(url, *args, **kwargs):
+def extract_meta_data(video, output_file, *args, **kwargs):
 
     """
 
     #TODO
     
     - Fill this with the documentation of the code. i.e. the docstring.
-    - Find a way to validate the url, return error if the URL is not in format. 
+    - Accomadate for the different video file types such as mp4, mkv, raw, etc.
 
-    - Add arguments to accomadate for different cloud providers. 
+    - Add arguments to accomadate, if needed.
 
     - Add requirements to requirements.txt usign "pip freeze > requirements"
 
-    - Make downloads resumable if it has any network issues. 
+    - Make sure the meta data output file is human readable. 
 
     """
-
-
